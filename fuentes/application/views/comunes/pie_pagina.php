93fc9dee31c93efd6b264bfe6cff776a01faf6d8
proyectosbeta <proyectosbeta@debianJessie>
2015-10-10 13:11:29 -0300

    estructura inicial del sitio

---
+++ fuentes/application/views/comunes/pie_pagina.php
@@ -0,0 +1,15 @@
+<!-- Inicio de Pie de pagina -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', <?php echo GOOGLE_ANALYTICS_US; ?>, <?php echo NOMBRE_DOMINIO; ?>);
+  ga('send', 'pageview');
+
+</script>
+<div class="container">
+    Para agregar eventos, enviar un mail a <b><?php echo MAIL_ENVIO; ?></b>
+ </div>
+<!-- Fin de Pie de pagina -->
